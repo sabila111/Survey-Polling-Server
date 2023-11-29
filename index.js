f4ecf436c4050bc0322efc3448c75abb86387e42
sabila111 <sharminsharker2002@gmail.com>
2023-11-30 00:51:05 +0600

    added patch

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express()
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 // middleware
 app.use(cors())
@@ -24,22 +24,66 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
     await client.connect();
     const surveyCollection = client.db("surveyDB").collection("survey");
-  
-    app.get('/survey', async(req, res) =>{
-        const result = await  surveyCollection.find().toArray();
-        res.send(result);
+    const usersCollection = client.db("surveyDB").collection("users");
+
+    // users related api 
+
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
     })
 
-    app.post('/survey', async(req, res) =>{
+    app.post('/users', async (req, res) => {
+
+      const user = req.body;
+      const query = {email: user.email}
+      const extistingUser = await usersCollection.findOne(query)
+      if(extistingUser){
+        return res.send({message : 'user created', insertedId: null})
+      }
+      const result = await usersCollection.insertOne(user)
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.patch('/users/surveyor/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: 'surveyor'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
+    app.get('/survey', async (req, res) => {
+      const result = await surveyCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.post('/survey', async (req, res) => {
 
       const job = req.body;
       console.log(job)
       const result = await surveyCollection.insertOne(job)
       res.send(result)
-     })
+    })
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -52,9 +96,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('food restaurant is available')
-  })
-  
-  app.listen(port, () => {
-    console.log(`food restaurant is running on port ${port}`)
-  })
+  res.send('food restaurant is available')
+})
+
+app.listen(port, () => {
+  console.log(`food restaurant is running on port ${port}`)
+})
